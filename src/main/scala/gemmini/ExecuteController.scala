3a15318d712a284d8a5c3f5412137854a68feaab
Seah Kim <kimsea8209@a7.Millennium.Berkeley.EDU>
2020-07-05 16:46:58 -0700

    fixed bug

--- src/main/scala/gemmini/ExecuteController.scala
+++ src/main/scala/gemmini/ExecuteController.scala
@@ -52,7 +52,6 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
   val (cmd, _) = MultiHeadedQueue(io.cmd, ex_queue_length, cmd_q_heads)
   cmd.pop := 0.U
 
-  io.busy := cmd.valid(0) || matmul_in_progress // cmd.valid(0)
 
   io.solitary_preload := cmd.valid(0) && cmd.bits(0).cmd.inst.funct === PRELOAD_CMD && !cmd.valid(1)
 
@@ -157,6 +156,7 @@ class ExecuteController[T <: Data, U <: Data](xLen: Int, tagWidth: Int, config: 
 
   // val matmul_in_progress = mesh.io.tags_in_progress.map(_.rob_ids(0).valid).reduce(_ || _)
   val matmul_in_progress = mesh.io.tags_in_progress.map(_.rob_id.valid).reduce(_ || _)
+  io.busy := cmd.valid(0) || matmul_in_progress // cmd.valid(0)
 
   // SRAM scratchpad
   // Fire counters which resolve same-bank accesses
